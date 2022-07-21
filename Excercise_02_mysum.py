c1f9c02cff462b21b13afc951f541cdea64ae1e8
mjanowsk <janowski1985@gmail.com>
2022-07-21 15:42:41 +0200

    updated code - v2

--- Excercise_02_mysum.py
+++ Excercise_02_mysum.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Excercise 2:
 
 def mysum (*args):
@@ -8,3 +10,10 @@ def mysum (*args):
         add += argument
     
     return add
+
+# function test:
+
+print(f'Sum of 1+5+7 is {mysum(1,5,7)}')
+
+
+
