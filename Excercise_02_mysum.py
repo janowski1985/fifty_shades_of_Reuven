4cd41f86ac4b4ce2af277583742488facd72627a
mjanowsk <janowski1985@gmail.com>
2022-07-23 12:40:57 +0200

    Added comment about iterating over the list

--- Excercise_02_mysum.py
+++ Excercise_02_mysum.py
@@ -16,4 +16,6 @@ def mysum (*args):
 print(f'Sum of 1+5+7 is {mysum(1,5,7)}')
 
 
-
+# if we want to put a list of number into the function and then iterate over it we can do it
+# with "*" Example:
+# mysum(*[1, 2, 3, 4]) - star will tell Python to iterate over intigers inside the list. 
